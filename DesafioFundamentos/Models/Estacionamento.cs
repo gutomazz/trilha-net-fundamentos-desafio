19b7f1993698257ccd8af6ad74e6a35a2b014cf0
Gustavo Thomaz <gutomazz@gmail.com>
2023-12-22 19:08:52 -0300

    DesafioFinal

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,3 +1,5 @@
+using System.Runtime.Intrinsics.Arm;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
@@ -14,34 +16,32 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
-            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            Console.WriteLine("Digite a placa do veículo para estacionar:"); // Pedi para o user digitar a placa
+            string novaPlaca = Console.ReadLine();                               // Criei uma string lendo o que foi digitado pelo user
+            Console.WriteLine($"A {novaPlaca} foi adicionada");                  // Informado ao cliente que a placa foi adcionada com sucesso!
+            veiculos.Add(novaPlaca);                                             // Placa adicionada a lista de veiculos
         }
 
         public void RemoverVeiculo()
         {
-            Console.WriteLine("Digite a placa do veículo para remover:");
+            Console.WriteLine("Digite a placa do veículo para remover:");   // user digita a placa
+            string placa = Console.ReadLine();                              // placa digitada adicionada a string "placa"   
+            
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
 
-            // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:"); // Pedido para digitar a QTD em horas
+                    string horas = Console.ReadLine();                                                   // leitura do que foi digitado
+                    int novaHora = int.Parse(horas);                                                     // add o inteiro da variavel horas na nova hora
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                    decimal valorTotal = (precoInicial+precoPorHora)*novaHora;                          // calculo
+                        veiculos[0]= placa;                                                              // removendo o video da lista 
+                        veiculos.RemoveAt(0);                                                           // removendo o video da lista 
 
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}"); //informado o valor 
+            
             }
             else
             {
@@ -49,14 +49,16 @@ namespace DesafioFundamentos.Models
             }
         }
 
-        public void ListarVeiculos()
+   public void ListarVeiculos()
         {
-            // Verifica se há veículos no estacionamento
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                
+               {foreach(string placa in veiculos)
+               Console.WriteLine(placa);}
+               
+                
             }
             else
             {
--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # DIO - Trilha .NET - Fundamentos
-www.dio.me
+Desafio realizado por - Gustavo Thomaz
 
 ## Desafio de projeto
-Para este desafio, você precisará usar seus conhecimentos adquiridos no módulo de fundamentos, da trilha .NET da DIO.
-
-## Contexto
-Você foi contratado para construir um sistema para um estacionamento, que será usado para gerenciar os veículos estacionados e realizar suas operações, como por exemplo adicionar um veículo, remover um veículo (e exibir o valor cobrado durante o período) e listar os veículos.
+Fundamentos de .net utilizando o C#. Abaixo deixo a proposta que foi estipulada:
 
 ## Proposta
 Você precisará construir uma classe chamada "Estacionamento", conforme o diagrama abaixo:
@@ -32,7 +29,3 @@ Por último, deverá ser feito um menu interativo com as seguintes ações imple
 2. Remover veículo
 3. Listar veículos
 4. Encerrar
-
-
-## Solução
-O código está pela metade, e você deverá dar continuidade obedecendo as regras descritas acima, para que no final, tenhamos um programa funcional. Procure pela palavra comentada "TODO" no código, em seguida, implemente conforme as regras acima.
